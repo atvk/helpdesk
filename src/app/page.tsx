53ec546b8c3af8f5ec40b2d96ee425441481a182
Anton Steblovskiy <steblovskiyanton@yandex.ru>
2024-01-28 14:41:50 +0300

    create form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,13 @@ import Link from "next/link"
 import ListingTable from "@/components/ListingTable"
 import Hydration from "@/components/Hydration";
 
+
 export default function adminPanel() {
   return (
     <Hydration>
       <Link href="./listings" className="m-10">Listins</Link>
       <ListingTable />
+    
     </Hydration>
   );
 }
--- src/components/ListingTable.tsx
+++ src/components/ListingTable.tsx
@@ -1,6 +1,7 @@
 "use client"
 import type { Listing } from "../domains/listing/types";
 import { useEffect, useState } from "react";
+import Link from "next/link"
 
 export default function ListingTable() {
 
@@ -20,8 +21,8 @@ export default function ListingTable() {
 
   return (
     <div className="flex gap-10 mx-10">
-      <div className="w-1/2">
-        <table className="min-w-full w-1/2 divide-y divide-gray-300">
+      <div className="w-full">
+        <table className="min-w-full divide-y divide-gray-300">
           <thead>
             <tr className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-700 sm:pl-0">
               <th scope="col" className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0">State{" "}</th>
@@ -44,10 +45,10 @@ export default function ListingTable() {
                       <td className="whitespace-nowrap py-5 text-sm text-gray-500">{listing.state}</td>
                       <td className="whitespace-nowrap py-5 text-sm text-gray-500">{listing.city_iata_code}</td>
                       <td className="whitespace-nowrap py-5 text-sm text-gray-500">{listing.created_datetime}</td>
-                      <td className="whitespace-nowrap py-5 text-sm text-gray-500">
-                        <div className="flex items-center">
-                          <div className="ml-4">
-                            <div className="flex font-medium gap-1">
+                      <td className="whitespace-nowrap py-5 text-sm text-gray-900">
+                        <div className="">
+                          <div className="">
+                            <div className="flex gap-1">
                               <span>{listing.first_name}</span>
                               <span>{listing.last_name}</span>
                             </div>
@@ -78,18 +79,95 @@ export default function ListingTable() {
           </tbody>
         </table>
       </div>{detailsState !== null ? (
-        <article className="w-1/2">
-          <h2>Listing ID: {detailsState.id} </h2>
-          <button
-            type="button"
-            className="inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            onClick={() => {
-              setDetailsState(null);
-            }}
-          >
-            Close
-          </button>
-        </article>
+        <form className="w-full">
+          <div className="space-y-10 sm:space-y-50">
+            <section>
+              <div className="flex justify-between mx-10 text-left text-sm font-semibold text-gray-700 sm:pl-0">
+                <div>
+                  <h2 className="text-base font-semibold leading-7 text-gray-900">Listing ID: {detailsState.id} </h2>
+                  <p className="max-w-2xl text-sm leading-6 text-gray-600">
+                    Submited:
+                  </p>
+                </div>
+                <div className="space-x-5">
+                  <button className="inline-flex items-center rounded-md bg-green-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green-900 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-900" >Approve</button>
+                  <button className="inline-flex items-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-900 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-900" >Reject</button>
+                </div>
+              </div>
+            </section>
+            <section>
+              <div className="grid mx-10 text-left text-sm font-semibold text-gray-700 sm:pl-0">
+
+                <h2 className="text-base font-semibold leading-7 text-gray-900">Post</h2>
+                <span className="max-w-2xl text-sm leading-6 text-gray-600">
+                  ID: {detailsState.id}
+                </span>
+                <span className="max-w-2xl text-sm leading-6 text-gray-600">
+                  First name: {detailsState.first_name}
+                </span>
+                <span className="max-w-2xl text-sm leading-6 text-gray-600">
+                  Last name: {detailsState.last_name}
+                </span>
+                <span className="max-w-2xl text-sm leading-6 text-gray-600">
+                  Telegram:
+                </span>
+                <span className="max-w-2xl text-sm leading-6 text-gray-600">
+                  Contact:
+                </span>
+                <div>
+                  <span className="max-w-2xl text-sm leading-6 text-gray-600">
+                    GUIDED FLOW(TELEGRAM ID)
+                  </span>
+                  <span className="max-w-2xl text-sm leading-6 text-gray-600">
+                    12345678910
+                  </span>
+                </div>
+              </div>
+            </section>
+            <section>
+              <div className="grid mx-10 text-left text-sm font-semibold text-gray-700 sm:pl-0">
+                <h2 className="text-base font-semibold leading-7 text-gray-900">Location</h2>
+                <span className="max-w-2xl text-sm leading-6 text-gray-600">
+                  City IATA code
+                </span>
+                <span className="max-w-2xl text-sm leading-6 text-gray-600">
+                  Country Alpha 2 code
+                </span>
+                <span className="max-w-2xl text-sm leading-6 text-gray-600">
+                  Address
+                </span>
+                <span className="max-w-2xl text-sm leading-6 text-gray-600">
+                  Mapbox
+                </span>
+                <span className="max-w-2xl space-y-8 last:text-sm leading-6 text-gray-600">
+                  <Link
+                    className=" text-gray-600 pr-5 underline"
+                    href="https://en.wikipedia.org/wiki/List_of_airports_by_IATA_airport_code:_A">City codes (IATA)</Link>
+                  <span>and</span>
+                  <Link
+                    className=" text-gray-600 p-5 underline"
+                    href="https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2">Country codes (Alpha2)</Link>
+                </span>
+              </div>
+
+            </section>
+            <button
+              type="button"
+              className="inline-flex ml-10 items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              onClick={() => {
+                setDetailsState(null);
+              }}
+            >
+              Close
+            </button>
+          </div>
+
+
+
+
+
+        </form>
+
       ) : null}
     </div>
   )
